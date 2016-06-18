c9b4ceb8e0a9c5d96297d80faff8ed1116ef7089
Charles <GUITARPLRC@YAHOO.COM>
2016-06-18 10:02:29 -0500

    optimized images / changed some style errors

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 wow = new WOW({
-	mobile:false
-})
+      mobile:false
+    })
 
-wow.init();
-
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+    wow.init();
+    
+    // SMOOTH SCROLLING
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
