ec5c2fa9b4b2a13fba11c1b379c662eda3a6e9dc
Charles <GUITARPLRC@YAHOO.COM>
2016-07-29 15:50:25 -0500

    fixing form server validation

--- mail.php
+++ mail.php
@@ -1,11 +1,26 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$message = $_REQUEST['message'];
 $subject = "Contact Form";
 $content = "From: $name \n Message: $message";
 $header = "From: $email \r\n";
 $me = "guitarplrc@yahoo.com";
-mail($me, $subject, $content, $header) or die ("Error!");
-echo "Thank you!";
+
+if ($name === "") :
+  echo "<div>Sorry, your name is a required field.</div>";
+endif; // name field empty
+
+if ($email === "") :
+  echo "<div>Sorry, your email is a required field.</div>";
+endif; // email field empty
+
+if ( !(preg_match('[A-Za-z]+', $name)) ):
+  echo "<div>Sorry, your name must only contain letters</div>";
+endif;
+
+mail($me, $subject, $content, $header) or die("Error!");
+echo "Thank you! Your message has been sent."
+
 ?>
