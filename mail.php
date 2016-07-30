21946f79f0f572a5efafa702d403c363f3c7cb77
Charles <GUITARPLRC@YAHOO.COM>
2016-07-30 11:03:52 -0500

    added server form check

--- mail.php
+++ mail.php
@@ -3,24 +3,27 @@
 $name = $_REQUEST['name'];
 $email = $_REQUEST['email'];
 $message = $_REQUEST['message'];
+$human = $_REQUEST['human'];
 $subject = "Contact Form";
 $content = "From: $name \n Message: $message";
 $header = "From: $email \r\n";
 $me = "guitarplrc@yahoo.com";
 
-if ($name === "") :
+if ($name === "") {
   echo "<div>Sorry, your name is a required field.</div>";
-endif; // name field empty
-
-if ($email === "") :
+} // name field empty
+else if ($email === "") {
   echo "<div>Sorry, your email is a required field.</div>";
-endif; // email field empty
-
-if ( !(preg_match('[A-Za-z]+', $name)) ):
-  echo "<div>Sorry, your name must only contain letters</div>";
-endif;
-
-mail($me, $subject, $content, $header) or die("Error!");
-echo "Thank you! Your message has been sent."
+} // email field empty
+else if ($message === "") {
+  echo "<div>Please include a message.</div>"
+}
+else if ($human != "4") {
+  echo "<div>Sorry, your multiplication needs some work.</div>";
+} // custom captcha answer
+else {
+  mail($me, $subject, $content, $header) or die("Error!");
+  echo "Thank you! Your message has been sent.";
+}
 
 ?>
